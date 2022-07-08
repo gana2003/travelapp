81c50b02f10de522a148d1a882bacf3b30c8e5ec
gana2003 <meenakshigana0@gmail.com>
2022-07-08 11:20:26 +0530

    travelapp

---
+++ src/app/travelregister/travelregister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travelregister',
+  templateUrl: './travelregister.component.html',
+  styleUrls: ['./travelregister.component.css']
+})
+export class TravelregisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
